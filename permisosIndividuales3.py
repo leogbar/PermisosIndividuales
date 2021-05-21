ba4b3183f37d2799a9e4e70ddc048032591db624
Leo Bar <leogbar@yahoo.com.ar>
2021-05-21 16:34:34 -0300

    Update permisosIndividuales3.py

--- permisosIndividuales3.py
+++ permisosIndividuales3.py
@@ -1,4 +1,17 @@
-import PyPDF2 as pypdf #https://mstamy2.github.io/PyPDF2/
+#-------------------------------------------------------------------------------
+# Name:        PermisosIndividuales
+# Purpose: Emisión de PDF con permisos individuales en funcion de un PDF mayor 
+# con ciertas caracteristicas, al cual, hay que borrarle y agregarle cosas en una
+# pagina y agregarle paginas modelo.
+#
+# Author:      Ing. Esp. Leonardo Barenghi
+#
+# Created:     21/05/2021
+# Copyright:   (c) lbarenghi 2021
+# Licence:     BSD 3
+#-------------------------------------------------------------------------------
+
+import PyPDF2 as pypdf #https://mstamy2.github.io/PyPDF2/ o https://github.com/mstamy2/PyPDF2
 
 with open("modelo.pdf", "rb") as overlay, open("infder1.pdf", "rb") as infder1, open("final.pdf", "rb") as final, open("sirfrar.pdf", "rb") as recorrer, open("nombres.txt", "r") as nombres:
 
@@ -16,7 +29,9 @@ with open("modelo.pdf", "rb") as overlay, open("infder1.pdf", "rb") as infder1, 
             licencia = pypdf.PdfFileWriter()
             licencia.addPage(licencia1)
             licencia.addPage(final1)
+            #archivo=archivo[:-1] # Sacar comentario si se ejecuta en windows que no quita el "\n" en la concatenacion de strings
             salida=str(x)+"_"+archivo+".pdf"
             x=x+1
+            prints("Imprimiendo el archivo: 
             with open(salida, "wb") as outFile:
                 licencia.write(outFile)
