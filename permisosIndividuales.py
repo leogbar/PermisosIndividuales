9e95721085cb9b875fc190b4406a4fa8e893d02c
Leo Bar <leogbar@yahoo.com.ar>
2021-05-26 20:39:34 -0300

    Update permisosIndividuales.py
    Se agrego unidecode para los nombres de archivo

--- permisosIndividuales.py
+++ permisosIndividuales.py
@@ -1,27 +1,28 @@
 #-------------------------------------------------------------------------------
-# Name:        PermisosIndividuales v4
+# Name:        PermisosIndividuales v5
 # Purpose: Emisión de PDF con permisos individuales en funcion de un PDF mayor 
 # con ciertas caracteristicas, al cual, hay que borrarle y agregarle cosas en una
 # pagina y agregarle paginas modelo.
 #
 # Author:      Ing. Esp. Leonardo Barenghi
 #
-# Created:     21/05/2021
+# Created:     26/05/2021
 # Copyright:   (c) lbarenghi 2021
 # Licence:     BSD 3
 #-------------------------------------------------------------------------------
 
 import PyPDF2 as pypdf #https://mstamy2.github.io/PyPDF2/
+from unidecode import unidecode  #(c) Tomaz Solc (GPLv2+) (GPL) https://pypi.org/project/Unidecode/ https://github.com/avian2/unidecode
 
-with open("fusionar.pdf", "rb") as fusionar, open("borrar.pdf", "rb") as borrar, open("adjunto.pdf", "rb") as final, open("sirfrar.pdf", "rb") as recorrer, open("nombres.txt", "r") as nombres:
+with open("fusionar.pdf", "rb") as fusionar, open("borrar.pdf", "rb") as borrar, open("adjunto.pdf", "rb") as final, open("sirfrar.pdf", "rb") as recorrer, open("nombres.txt","r", encoding="utf-8") as nombres:
 
-printf("PermisosIndividuales v4\n")
-printf("Emisión de PDF con permisos individuales\n")
-printf("Author: Ing. Esp. Leonardo Barenghi\n")
-printf("Created:     21/05/2021\n")
-printf("Modified:     26/05/2021\n")
-printf("Copyright:   (c) lbarenghi 2021\n")
-printf("Licence:     BSD 3\n")
+    print("PermisosIndividuales v4\n")
+    print("Emisión de PDF con permisos individuales\n")
+    print("Author: Ing. Esp. Leonardo Barenghi\n")
+    print("Created:     21/05/2021\n")
+    print("Modified:     26/05/2021\n")
+    print("Copyright:   (c) lbarenghi 2021\n")
+    print("Licence:     BSD 3\n")
 
 
 
@@ -39,7 +40,8 @@ printf("Licence:     BSD 3\n")
             licencia = pypdf.PdfFileWriter()
             licencia.addPage(licencia1)
             licencia.addPage(final1)
-            #archivo=archivo[:-1] # Sacar comentario si se ejecuta en windows que no quita el "\n" en la concatenacion de strings
+            archivo = unidecode(archivo)
+            archivo=archivo[:-1]
             salida=str(x)+"_"+archivo+".pdf"
             x=x+1
             print("Generando "+salida+ "..."+"\n")
